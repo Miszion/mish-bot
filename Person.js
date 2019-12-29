f4e1f3da740db0816bb079dfb24402f222151669
Mission Marcus <missionmarcus@gmail.com>
2019-12-28 20:15:05 -0600

    initialized custom text

--- Person.js
+++ Person.js
@@ -1,45 +1,35 @@
 module.exports = class Person {
 
-    constructor(name, day, choreList, phoneNumber) {
+    constructor(name, dayList, choreList, phoneNumber) {
         this.name = name;
         
-        if (day == 1) {
-            this.dayString = 'Monday';
-        }
-        else if (day == 2) {
-            this.dayString = 'Tuesday';
-        }
-        else if (day == 3) {
-            this.dayString = 'Wednesday';
-        }
-        else if (day == 4 ) {
-            this.dayString = 'Thursday';
-        }
-        else if (day == 5) {
-            this.dayString = 'Friday';
-        }
-        else if (day == 6) {
-            this.dayString = 'Saturday';
-        }
-        else {
-            this.dayString = 'Sunday';
-        }
-
-        this.day = day;
-
+        this.dayList = dayList;
+        this.day = "";
         this.choreList = choreList;
         this.phoneNumber = phoneNumber;
 
     }
 
-    getDay() {
-        return this.day;
+    hasDay(number) {
+        
+        for (x in this.dayList) {
+            if (x == number){
+                this.day = number;
+
+                if (this.dayList.length == 2) {
+                    this.selectedIndex = x;
+                }
+
+                return true;
+            }
+        }
+
     }
 
 
     getInformation() {
 
-        return 'Good Morning, ' + this.name + '.\nYour chores are: ' + (this.choreList.length == 2 ? this.choreList.join(' and ') + "." : this.choreList.join(' , '));
+        return `Good Morning, ${this.name}.\nToday is ${this.day}. Your zone(s) are: ${ (this.dayList.length == 1 ? this.choreList.join(' and ') : this.choreList[this.selectedIndex])`;
 
 
     }
--- app.js
+++ app.js
@@ -14,14 +14,13 @@ app.use(express.json());
 var port = process.env.PORT || 3000;
 
 
-const donna = new Person("Donna", 0, ["Upstairs"], "+13127314590");
-const luie = new Person("Baggin", 1, ["Basement", "Kitchen"], "+17737479316");
-const dwayne = new Person("Dwayne", 1, ["Living Room", "Bathroom"], "+13122412632");
-const nathan = new Person("Nathan", 3, ["Basement", "Bathroom"], "+13124048334");
-const beto = new Person("Beto", 3, ["Upstairs", "Kitchen"], "+13124801330");
-const mission = new Person("Mission", 5, ["Basement", "Kitchen"], "+17736276985");
-const donna2 = new Person("Donna", 5, ["Living Room"], "+13127314590");
-const jim = new Person("Jim", 6, ["Living Room", "Bathroom"], "+17738186686");
+const donna = new Person("Donna", [0, 5], ["Upstairs", "Living Room"], "+13127314590");
+const luie = new Person("Baggin", [1], ["Basement", "Kitchen"], "+17737479316");
+const dwayne = new Person("Dwayne", [1], ["Living Room", "Bathroom"], "+13122412632");
+const nathan = new Person("Nathan", [3], ["Basement", "Bathroom"], "+13124048334");
+const beto = new Person("Beto", [3], ["Upstairs", "Kitchen"], "+13124801330");
+const mission = new Person("Mission", [5], ["Basement", "Kitchen"], "+17736276985");
+const jim = new Person("Jim", [6], ["Living Room", "Bathroom"], "+17738186686");
 
 const coda = new Person("Coda", 0, [], "+17737277293");
 const geneva = new Person("Geneva", 4, ["Living Room", "Kitchen"], "+17736822576")
@@ -90,7 +89,7 @@ function delegate() {
   
       choreList.forEach(function(x) {
     
-      if (x.getDay() === dayNumber) {
+      if (x.hasDay(dayNumber)) {
   
           sendMessage(x);
   

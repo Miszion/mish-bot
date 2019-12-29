dc0381360c6542a7abe13c8e89622ed81ee2b441
Mission Marcus <missionmarcus@gmail.com>
2019-12-28 20:47:33 -0600

    updated test

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.post('*', (req, res) => {
 
   }
   else if (req.body.Body.toLowerCase().trim() == 'test'){
-    const day = 1;
+    const day = req.body.Body.toLowerCase().trim().substring(4, req.body.Body.length);
 
       choreList.forEach(function(x) {
     
